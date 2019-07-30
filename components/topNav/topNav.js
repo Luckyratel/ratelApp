c928131a805503953f23f78a4e86d87ea80f7402
letong <1047816691@qq.com>
2019-07-30 16:07:03 +0800

    基本顶部导航栏已经完成

--- components/topNav/topNav.js
+++ components/topNav/topNav.js
@@ -4,20 +4,54 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    list:Array,
+    defaultColor:{
+      type:String,
+      value:"#444444",
+    },
+    activeColor:{
+      type:String,
+      value:"green",
+    },
+    lineStyle: {
+      type: Boolean,
+      value: false,
+    },
+    selectIndex:{
+      type:Number,
+      value:0,
+    }
+  },
+  ready:function(){
+    console.log(this.properties.list);
+    var length = this.properties.list.length;
+    this.setData({
+      itemWidth: (100 / length) + '%',
+      selectTab: this.properties.selectIndex,
+    })
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    selecColor:"#444444",
+    selectTab: 1,
+    itemWidth: "",
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    selectTab:function(e){
+      var index = e.currentTarget.dataset.index;
+      index = parseInt(index);
+      var item = this.data.list[index];
+      this.setData({
+        selectTab: index,
+      })
+      this.triggerEvent("selectTab",{index: index,item: item});
+    },
   }
 })
--- pages/display/topNav/topNav.js
+++ pages/display/topNav/topNav.js
@@ -5,14 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    list:['全部','待付款','待收货','已收货'],
+    lineStyle: false,
+    title:"",
+    selectIndex: 0,
+    selectItem:"",
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var list = this.data.list;
+    var selectIndex = this.data.selectIndex;
+    this.setSelectItem(this.data.lineStyle)
+    this.setData({
+      selectItem: list[selectIndex],
+    })
   },
 
   /**
@@ -28,39 +37,33 @@ Page({
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  // 选择事件
+  selectTab:function(e){
+    console.log(e);
+    this.setData({
+      selectItem: e.detail.item,
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  // 选择是否有下滑线效果
+  selectLine:function(){
+    this.setSelectItem(!this.data.lineStyle);
+    this.setData({
+      lineStyle: !this.data.lineStyle,
+    })
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 工具类
+  setSelectItem:function(type){
+    if (this.type) {
+      this.setData({
+        title: "无下划线",
+      })
+    }
+    else {
+      this.setData({
+        title: "有下划线",
+      })
+    }
   }
 })
